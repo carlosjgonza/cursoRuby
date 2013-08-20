50d73d1e55ede903e9dec2deb6ab601dd3fd81f3
Carlos <carlosj.gonzalez28@gmail.com>
2013-08-20 11:21:43 -0430

    Agregando Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ CursoRuby::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'sitio#index'
+  root to: 'sitio#inicio'
+
+  get 'acerca' => 'sitio#acerca'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
