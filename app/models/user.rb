b4a5b567eeaab4bca00af1d2277affb876629bdb
Carlos <carlosj.gonzalez28@gmail.com>
2013-08-26 11:04:36 -0430

    Vinculando Recursos con Usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable  
+
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable  
+
+  has_many :pins
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
