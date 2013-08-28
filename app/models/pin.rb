0b93272cde15bdfab2a52ed12813e9849956b0b3
Carlos <carlosj.gonzalez28@gmail.com>
2013-08-28 09:51:43 -0430

    Agregando Servicio Amazon S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,6 @@
 class Pin < ActiveRecord::Base		
 
-	has_attached_file :image,
-	 url: "/system/:attachment/:id/:style/:basename.:extension",
-	 path: ":rails_root/public/system/:attachment/:id/:style/:basename.:extension",
-	 styles: { medium: "320x240>"} 
+	has_attached_file :image, styles: { medium: "320x240>"} 
 
 	validates :description, presence: true
 	validates_attachment :image, presence: true,
