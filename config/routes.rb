585344ff261cc5f25e1ecab94fe44b39ae3d9997
Carlos <carlosj.gonzalez28@gmail.com>
2013-08-28 10:56:15 -0430

    Cambiando nuestro index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ CursoRuby::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'sitio#inicio'
+  root to: 'pins#index'
 
   get 'acerca' => 'sitio#acerca'
 
