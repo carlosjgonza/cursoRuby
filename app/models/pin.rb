a87c8fc022df5773cc093ecde39792cfd8b0744b
Carlos <carlosj.gonzalez28@gmail.com>
2013-08-29 10:34:03 -0430

    Agregando Imagenes Via url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,12 @@ class Pin < ActiveRecord::Base
 						content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
 						size: { less_than: 5.megabytes }
 
-	attr_accessible :user_id, :description, :image
+	attr_accessible :user_id, :description, :image, :image_remote_url
 
 	belongs_to :user	
+
+	def image_remote_url=(url_value)
+		self.image = URI.parse(url_value) unless url_value.blank?
+		super
+	end
 end
